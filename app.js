bc126fed0d2bb0308193fafa98df08566ef10a32
STARxiao7 <147518918+STARxiao7@users.noreply.github.com>
2024-10-15 04:55:38 +0800

    Update app.js

--- app.js
+++ app.js
@@ -23,8 +23,7 @@ var app = express();
 
 const secretKey = 'match280961'
 
-app.set('secretKey',secretKey)
-
+app.set('secretKey', secretKey)
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -32,38 +31,39 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(expressJWT({ secret: secretKey, algorithms: ['HS256'] }).unless({ path: [/^\/apiG\//] }))
+app.use('/test', user)
 
-app.use(expressJWT( { secret: secretKey, algorithms:['HS256']}).unless( { path: [/^\/apiG\//] } ))
-app.use('/test',user)
+app.use('/apiG/guestLogin', guestLogin)
+app.use('/api/getUser', getUser)
+app.use('/api/randomMenu', randomMenu)
+app.use('/api/addMenu', addMenu)
+app.use('/apiG/communityList', communityList)
+app.use('/apiG/register', register)
+app.use('/apiG/login', login)
+app.use('/api/personalMenu', personalMenu)
+app.use('/api/deleteMenu', deleteMenu)
+app.use('/apiG/typeList', typeList)
 
-
-app.use('/apiG/guestLogin',guestLogin)
-app.use('/api/getUser',getUser)
-app.use('/api/randomMenu',randomMenu)
-app.use('/api/addMenu',addMenu)
-app.use('/apiG/communityList',communityList)
-app.use('/apiG/register',register)
-app.use('/apiG/login',login)
-app.use('/api/personalMenu',personalMenu)
-app.use('/api/deleteMenu',deleteMenu)
-app.use('/apiG/typeList',typeList)
-
-
-
-
-app.use((err,req,res,next) => {
+app.use((err, req, res, next) => {
     //'UnauthorizedError'.如果捕获了前面这个字段的错误，则说明时token解析发生了错误
     if (err.name === 'UnauthorizedError') {
         res.status(401)
-        res.send({status:401,msg:'无效的token'})
-    }else{
+        res.send({ status: 401, msg: '无效的token' })
+    } else {
         console.log(err.message)
         res.status(500)
         res.send({
             status: 500,
-            msg:'发生未知错误'
+            msg: '发生未知错误'
         })
     }
-
 })
+
+// 监听端口，使用 Railway 提供的 PORT 环境变量或默认 3000
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
 module.exports = app;
